f719cfd7309ceeb349557d62a9ce0c0cbea029e9
Adhidarma Hadiwinoto <gua@adhisimon.or.id>
2011-05-25 20:08:06 +0700

    prepare for windows

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from cx_Freeze import setup, Executable
+
+exe = Executable(
+    script = "bijinclock.py",
+    base = "Win32GUI"
+)
+setup(
+    name = "bijinclock",
+    version = "0.9.9",
+    description = "Bijin Clock",
+    executables = [exe],
+    options = {
+        "build_exe": {
+            "include_files": [
+                "gtkrc",
+                ("gtk+_2.22.1-1_win32/share/themes", "share/themes"),
+                ("gtk+_2.22.1-1_win32/lib", "lib"),
+                ("gtk+_2.22.1-1_win32/etc", "etc"),
+            ],
+            "icon": "bijinclock.ico"
+        }
+    }
+)
