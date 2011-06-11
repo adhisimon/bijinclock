f11ba2548b271986c3bd3dc9eb5b06d5a2a1ce9d
Adhidarma Hadiwinoto <gua@adhisimon.or.id>
2011-06-11 10:06:04 +0700

    remove width and height information from source

--- bijinclock.py
+++ bijinclock.py
@@ -24,83 +24,60 @@ from time import sleep
 
 class Bijin:
     version = '1.1.0.dev'
-    url_template = 'http://www.clockm.com/tw/widget/img/clk/hour/%H%M.jpg'
     source_urls = {
         0: {
             'title': 'ClockM - small',
             'url': 'http://www.clockm.com/tw/widget/img/clk/hour/%H%M.jpg',
-            'width': 160,
-            'height': 240,
             'http-referer': 'https://github.com/adhisimon/bijinclock/wiki',
         },
         1: {
             'title': 'ClockM - Big',
             'url': 'http://www.clockm.com/tw/img/clk/hour/%H%M.jpg',
-            'width': 399,
-            'height': 600,
             'http-referer': 'https://github.com/adhisimon/bijinclock/wiki',
         },
         2: {
             'title': 'Arthur',
             'url': 'http://www.arthur.com.tw/photo/images/400/%H%M.JPG',
-            'width': 320,
-            'height': 480,
             'http-referer': 'https://github.com/adhisimon/bijinclock/wiki',
         },
         3: {
             'title': 'Bijin Tokei - Japan - Small',
             'url': 'http://www.bijint.com/assets/pict/bijin/240x320/%H%M.jpg',
-            'width': 240,
-            'height': 320,
             'http-referer': 'http://www.bijint.com/jp/',
         },
         4: {
             'title': 'Bijin Tokei - Japan - Big',
             'url': 'http://bijint.com/jp/tokei_images/%H%M.jpg',
-            'width': 590,
-            'height': 450,
             'http-referer': 'http://www.bijint.com/jp/',
         },
         5: {
             'title': 'Bijin Tokei - Korea',
             'url': 'http://www.bijint.com/assets/pict/kr/240x320/%H%M.jpg',
-            'width': 240,
-            'height': 320,
             'http-referer': 'http://www.bijint.com/kr/',
         },
         6: {
             'title': 'Bijin Tokei - Hong Kong',
             'url': 'http://www.bijint.com/assets/pict/hk/240x320/%H%M.jpg',
-            'width': 240,
-            'height': 320,
             'http-referer': 'http://www.bijint.com/hk/',
         },
         7: {
             'title': 'Gal Tokei',
             'url': 'http://gal.bijint.com/assets/pict/gal/240x320/%H%M.jpg',
-            'width': 240,
-            'height': 320,
             'http-referer': 'http://gal.bijint.com/',
         },
         8: {
             'title': 'Circuit Tokei',
             'url': 'http://www.bijint.com/assets/pict/cc/590x450/%H%M.jpg',
-            'width': 590,
-            'height': 450,
             'http-referer': 'http://www.bijint.com/cc/',
         },
         9: {
             'title': 'Binan Tokei',
             'url': 'http://www.bijint.com/assets/pict/jp/240x320/%H%M.jpg',
-            'width': 240,
-            'height': 320,
             'http-referer': 'http://www.bijint.com/binan/',
         },
         10: {
             'title': 'Lovely Time II',
             'url': 'http://gameflier.lovelytime.com.tw/photo/%H%M.JPG',
-            'width': 320,
-            'height': 480,
             'http-referer': 'https://github.com/adhisimon/bijinclock/wiki',
         },
     }
@@ -379,7 +356,6 @@ Special thanks to:
         else:
             time_to_show = time(hour = hour, minute = minute)
 
-        #return time_to_show.strftime(self.url_template)
         return time_to_show.strftime(self.source_urls[self.source_selected]['url'])
 
     def url_to_show_next(self):
@@ -389,10 +365,6 @@ Special thanks to:
         return self.url_to_show(next_minute.hour, next_minute.minute)
 
     def replace_image(self, filename):
-        #self.window.set_size_request(
-        #    self.source_urls[self.source_selected]['width'],
-        #    self.source_urls[self.source_selected]['height']
-        #)
         self.message_label.hide()
         self.clock_image.set_from_file(filename)
         self.last_source_selected = self.source_selected
